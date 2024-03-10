cf0fa8ee95b78cde4c9644efa500958e79c419f3
unknown <bardhoshieugen2@gmail.com>
2024-03-10 19:06:24 +0000

    changes in github

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,9 @@ function App() {
 
   return (
     <>
-      <h1 className="site-title">A Song of Fire and Ice</h1>
       <CharacterGallery />
     </>
   );
 }
 
-export default App
+export default App;
